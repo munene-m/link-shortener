5220655c9c4afce5389252dc671d2f4d17549619
munene-m <munenenjue18@gmail.com>
2023-12-22 13:59:11 +0300

    chore: add dependency array to run useeffect once

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -16,7 +16,7 @@ export default function Link() {
   }
   useEffect(() => {
     redirectToOriginal();
-  });
+  }, []);
   return (
     <div className="h-screen">
       <h1 className="text-white text-center">Redirecting...</h1>
